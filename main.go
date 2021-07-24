533eb356c06432de51fd4417127fc7f73b80c671
Jim Male <2423859+jimmale@users.noreply.github.com>
2021-07-24 14:19:22 -0500

    complied with Apache License

--- main.go
+++ main.go
@@ -10,12 +10,28 @@ temperature-sensor,chip=28-00000a6999ff temperature=-22.19 1603911540198853959
 */
 
 import (
+	_ "embed"
 	"fmt"
 	"github.com/yryz/ds18b20"
+	"os"
 	"time"
 )
 
+//go:embed LICENSE
+var LICENSE string
+
 func main() {
+	if len(os.Args) > 1 {
+		if os.Args[1] == "--license" || os.Args[1] == "--licence" {
+			fmt.Println("This application contains libraries from http://github.com/yryz/ds18b20 and is")
+			fmt.Println("licensed to you under the terms of the Apache License:\n")
+			fmt.Println("================================================================================")
+			fmt.Println(LICENSE)
+			fmt.Println("================================================================================")
+			os.Exit(0)
+		}
+	}
+
 	now := time.Now().UnixNano()
 	sensors, err := ds18b20.Sensors()
 	if err != nil {
